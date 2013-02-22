824edf4bd1f673582c4fc21d36346efb97a1bcdd
Pascal d'Hermilly <pascal@dhermilly.dk>
2013-02-22 01:06:46 +0100

    Added an API function to client frontend and created/implemented a json-format to talk to the API server script

--- frontend/api.php
+++ frontend/api.php
@@ -1,25 +1,34 @@
 <?php
-if (!isset($_GET["action"])){
-	exit("You need to specify an action.");
-}
+try {
+	$response = array("payload"=>null);
+	if (!isset($_GET["action"])){
+		throw new Exception("You need to specify an action.");
+	}
 
-require_once "../backend/database.php";
-if ($_GET["action"]=="getMemberList"){
-	exit(json_encode(getAllMembers()));
-} else if ($_GET["action"]=="addNewMember"){
-	$db = connect();
-	$member = $_GET;
-	unset($member['action']);
-	addNewMember($member);
-} else if ($_GET["action"]=="updateMember"){
-	$db = connect();
-	$member = $_GET;
-	unset($member['action']);
-	addNewMember($member);
-} else if ($_GET["action"]=="deleteMember"){
-    deleteMember($_GET['id']);
-} else {
-	exit("Action not known");
+	require_once "../backend/database.php";
+	if ($_GET["action"]=="getMemberList"){
+		$payload = getAllMembers();
+	} else if ($_GET["action"]=="addNewMember"){
+		$db = connect();
+		$member = $_GET;
+		unset($member['action']);
+		addNewMember($member);
+	} else if ($_GET["action"]=="updateMember"){
+		$db = connect();
+		$member = $_GET;
+		unset($member['action']);
+		addNewMember($member);
+	} else if ($_GET["action"]=="deleteMember"){
+		deleteMember($_GET['id']);
+	} else {
+		throw new Exception("Action not known");
+	}
+	$response["error"] = false; 
+} catch (Exception $e){
+	$response["error"] = true;
+	$response["error_msg"] = $e->getMessage();
+	$response["error_code"]= $e->getCode();
 }
-
+$response["payload"] = $payload;
+exit(json_encode($response));
 ?>
--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,29 +1,48 @@
-function refreshMemberList(){
-	$.getJSON('api.php',{action:'getMemberList'},function(data){
-		var items = [];
-		$.each(data, function(key, val) {
-			items.push('<li id="' + key + '">' + val["name"] + ' - '+val["email_address"]+' <a href="javascript:void(0)" onclick="editMemberInitiate('+val["id"]+')">✎</a> <a href="javascript:void(0)" onclick="deleteMember('+val["id"]+')">❌</a></li>');
-		});
-		$('#memberList').replaceWith($('<ul/>',{id:'memberList',html: items.join('')}));
-	});
+function apiCall(action, func, args){
+//     console.log(args);
+    var params = {action:action};
+    if ($.isArray(args)){
+        for (var i=0;i<args.length;i++){
+            params[args[i].name] = args[i].value;
+        }
+    } else if ($.isPlainObject(args)){
+        $.each(args, function(key, val){
+            params[key] = val;
+        });
+    }
+    console.log(params);
+    $.getJSON('api.php',params,function (data){
+                if (data.error) {
+                    alert(data.error_msg);
+                } else {
+                    func(data);
+                }
+        });
+}
+function refreshMemberlist(){
+    apiCall('getMemberList',renderMemberList);
+}
+function renderMemberList(data){
+    var items = [];
+    $.each(data["payload"], function(key, val) {
+        items.push('<li id="' + key + '">' + val["name"] + ' - '+val["email_address"]+' <a href="javascript:void(0)" onclick="editMemberInitiate('+val["id"]+')">✎</a> <a href="javascript:void(0)" onclick="deleteMember('+val["id"]+')">❌</a></li>');
+    });
+    $('#memberList').replaceWith($('<ul/>',{id:'memberList',html: items.join('')}));
 }
 function init(){
-	refreshMemberList();
-	$('#addMemberForm').submit(function(){
-		$.get('api.php?action=addNewMember&' + $('#addMemberForm').serialize(),function(){
-			refreshMemberList();
-			$('#addMemberForm').trigger('reset')
-		});
-		return false;
-	})
+    refreshMemberlist()
+    $('#addMemberForm').submit(function(){
+        apiCall('addNewMember',function(){refreshMemberlist(),$('#addMemberForm').trigger('reset')},$('#addMemberForm').serializeArray());
+        return false;
+    });
 }
 function deleteMember(id){
-	if (confirm ("Are you sure you want to delete?")){
-		$.get('api.php?action=deleteMember&id='+id,refreshMemberList);
-	}
+    if (confirm ("Are you sure you want to delete?")){
+        apiCall('deleteMember',refreshMemberlist,{id:id})
+    }
 }
 function editMemberInitiate(id){
-	alert('You won the implementation :-)');
+    alert('You won the implementation :-)');
 }
 
 
